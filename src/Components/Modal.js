36cd3c29b263bed4625dc970c5274866d5fd0bdf
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2020-05-22 21:14:01 -0400

    converted modal component to hooks and added esc function

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,32 +1,36 @@
-import React from "react";
+import React, { useCallback, useEffect } from 'react'
 import Form from "./Form"
 
-export default class Modal extends React.Component {
+export default function Modal(props) {
 
-  constructor() {
-    super();
-    // Create ref to allow "close" button to be focused on when modal opens.
-    this.button = React.createRef();
-  }
+  const { toggleModal } = props;
 
-  // Focus on close button when modal opens
-  componentDidMount() {
-    this.button.current.focus();
-  }
+  
+  
+  const escFunction = useCallback((event) => {
+    if(event.keyCode === 27) {
+      toggleModal();
+    }
+  }, []);
 
-  render() {
-    const { toggleModal } = this.props;
-    return (
-      // Modal contents: close button, form, modal overlay for background
-      <React.Fragment>
-        <div className="modal">
-          <button ref={this.button} className="close-modal" aria-label="close form" onClick={toggleModal} tabIndex="0">
-            &times;
+  useEffect(() => {
+    document.addEventListener("keydown", escFunction, false);
+
+    return () => {
+      document.removeEventListener("keydown", escFunction, false);
+    };
+  }, []);
+
+  return (
+    // Modal contents: close button, form, modal overlay for background
+    <React.Fragment>
+      <div className="modal">
+        <button className="close-modal" aria-label="close form" tabIndex="0" onClick={toggleModal}>
+          &times;
           </button>
-          <Form />
-        </div>
-        <div className="modal-overlay" onClick={toggleModal}></div>
-      </React.Fragment>
-    );
-  }
+        <Form />
+      </div>
+      <div className="modal-overlay"></div>
+    </React.Fragment>
+  );
 }

b123ffdd9135c4c2af7594a8e59181ea8ffe3032
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2020-05-22 21:59:06 -0400

    added functionality to focus on close button when modal opens

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,12 +1,14 @@
-import React, { useCallback, useEffect } from 'react'
+import React, { useCallback, useEffect, useRef } from 'react'
 import Form from "./Form"
 
 export default function Modal(props) {
 
   const { toggleModal } = props;
 
+  // Create ref to target "close" button when modal opens
+  const refContainer = useRef(null);
   
-  
+  // Function to close modal when esc button is pressed
   const escFunction = useCallback((event) => {
     if(event.keyCode === 27) {
       toggleModal();
@@ -14,9 +16,15 @@ export default function Modal(props) {
   }, []);
 
   useEffect(() => {
+
+    // Event listener for esc button press
     document.addEventListener("keydown", escFunction, false);
 
+    // Focus on button when modal opens
+    refContainer.current.focus();
+
     return () => {
+      // Remove event listener for esc button press after modal closes
       document.removeEventListener("keydown", escFunction, false);
     };
   }, []);
@@ -25,7 +33,7 @@ export default function Modal(props) {
     // Modal contents: close button, form, modal overlay for background
     <React.Fragment>
       <div className="modal">
-        <button className="close-modal" aria-label="close form" tabIndex="0" onClick={toggleModal}>
+        <button ref={refContainer} className="close-modal" aria-label="close form" tabIndex="0" onClick={toggleModal}>
           &times;
           </button>
         <Form />

1c50c3583066020c3a3516eb5cc5bcff1cb396e3
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2022-03-26 14:26:38 -0400

    Updates

---
+++ precache-manifest.e228596ff54f078ff0a0bafe7413982e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975ff74336f33e07d5c6bef18f6bd990",
+    "url": "/my-user/index.html"
+  },
+  {
+    "revision": "a251e1eb12bcf5c083b4",
+    "url": "/my-user/static/css/2.0b0521d5.chunk.css"
+  },
+  {
+    "revision": "51d0262c679ae83e18cb",
+    "url": "/my-user/static/css/main.b9e376f7.chunk.css"
+  },
+  {
+    "revision": "a251e1eb12bcf5c083b4",
+    "url": "/my-user/static/js/2.11d9e35c.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/my-user/static/js/2.11d9e35c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51d0262c679ae83e18cb",
+    "url": "/my-user/static/js/main.6b20350e.chunk.js"
+  },
+  {
+    "revision": "6e2d4f3aca39d5193ee3",
+    "url": "/my-user/static/js/runtime-main.743df147.js"
+  }
+]);
